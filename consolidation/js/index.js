1ce2d11cca5424226f061a05ac581d57e009d812
Reneicko Getten <rkgetten@gmail.com>
2019-03-20 22:09:34 -0500

    intial files copy

--- consolidation/js/index.js
+++ consolidation/js/index.js
@@ -1,42 +1,75 @@
-$(document).ready(function(){
-  
-    $(".startdate").datepicker({
-        autoclose: true,
-    }).on('changeDate', function (selected) {
-        var minDate = new Date(selected.date.valueOf());
-        $('.enddate').datepicker('setStartDate', minDate);
-    });
-    
-    $(".enddate").datepicker()
-        .on('changeDate', function (selected) {
-            var minDate = new Date(selected.date.valueOf());
-            $('.startdate').datepicker('setEndDate', minDate);
-        });
+/**
+ * Loads campaign name modal
+ */
+function loadModal() {
+    $("#cname").modal('show');
+    $("input#campaign").focus();
+}
+
+/**
+ * Displays Alert upon Error
+ * @param {str} msg Message to display
+ */
+function errorAlert(msg) {
+    $(".errors").removeClass("hidden");
+    $(".errormsg").html(msg);
+}
 
 
+/**
+ * Validates the campaign name;
+ * @returns {boolean} True/False
+ */
+function isValidateCName() {
+    var root = v8n().string();
+    var empty=v8n().empty();
+    var rootClass = $(".form-text.campaignName").addClass("danger-text");
+
+    var campaignName = $("#campaign").val();
 
 
+    var errors = [root.minLength(4).test(campaignName), root.maxLength(30).test(campaignName)];
+    var isEmpty = empty.test(campaignName);
 
-        $('#myTable').DataTable( {
-            "ajax": "data/objects.txt",
-            "columns": [
-                { "data": "name" },
-                { "data": "position" },
-                { "data": "office" },
-                { "data": "extn" },
-                { "data": "start_date" },
-                { "data": "salary" }
-            ]
-        } );
-
-
-        $("#submit").click(function(){
-            var count=$("#count").val();
-
-            if(count > 5000)
+    for (var i = 0; i < errors.length; i++) {
+        if (errors[1] == !1) {
+            rootClass.html("Campaign name is too long. (30 Characters allowed.)");
+            return !1;
+        } else if (errors[0] == !1) {
+            if(isEmpty == !0)
             {
-                $(".errors").removeClass("hidden");
-                $(".errormsg").html("Count should be equal or less than 5000");
+                rootClass.html("Please enter a campaign name to continue.");
             }
-        })
+            else{
+                rootClass.html("Campaign name is too short.(It should not be less than 4 Characters.)")
+            }
+            
+            return !1;
+        }
+    }
+
+
+    return !0;
+
+}
+
+$(document).ready(function () {
+
+   // loadModal();
+
+    $('#continue').click(function () {
+       modalHandler(isValidateCName())
+    })
+
 });
+
+function modalHandler(isValidate)
+{
+if(isValidate == !0)
+{
+    var campaignName = $("#campaign").val();
+    $("#cname").modal('hide');
+    localStorage.setItem('campaignName',campaignName);
+}
+
+}
